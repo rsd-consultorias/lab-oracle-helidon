867027e0802ee1a31f90bec216aea7a13a5fb81b
Rafael Dias <rafael.dias@rsdconsultoria.com.br>
2024-09-21 13:14:43 -0300

    add lab

---
+++ lab-helidon/src/test/java/br/com/rsdconsultoria/lab/helidon/MainTest.java
@@ -0,0 +1,11 @@
+package br.com.rsdconsultoria.lab.helidon;
+
+import io.helidon.webserver.testing.junit5.DirectClient;
+import io.helidon.webserver.testing.junit5.RoutingTest;
+
+@RoutingTest
+class MainTest extends AbstractMainTest {
+    MainTest(DirectClient client) {
+        super(client);
+    }
+}
